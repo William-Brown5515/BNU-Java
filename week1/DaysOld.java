7d6123af98943838258bd9016920ca983de0a817
William-Brown5515 <william.brown@stfc.ac.uk>
2023-10-13 12:51:29 +0100

    06/10/2023 Exercise 7

---
+++ week1/DaysOld.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+
+public class DaysOld {
+    public static void main(String[] args) {
+        Scanner dob = new Scanner(System.in);
+        System.out.print("Please enter the day of your date of birth   ");
+        Integer birthDay = dob.nextInt();
+        System.out.print("Please enter the month of your date of birth   ");
+        Integer birthMonth = dob.nextInt();
+        System.out.print("Please enter the year of your date of birth   ");
+        Integer birthYear = dob.nextInt();
+        Calendar now = Calendar.getInstance();
+
+        Integer year = now.get(Calendar.YEAR);
+        Integer month = now.get(Calendar.MONTH);
+        Integer day = now.get(Calendar.DATE);
+
+        LocalDate dateBefore = LocalDate.of(birthYear, birthMonth, birthDay);
+        LocalDate dateAfter = LocalDate.of(year, month, day);
+        long noOfDaysBetween = ChronoUnit.DAYS.between(dateBefore, dateAfter);
+        System.out.println(noOfDaysBetween);
+        dob.close();
+    }
+}
