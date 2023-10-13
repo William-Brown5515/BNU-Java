deb868cff973d8276c94fad1a4353c7defbdacac
William-Brown5515 <william.brown@stfc.ac.uk>
2023-10-13 16:58:14 +0100

    20/10/2023 Exercise 7

--- Java OOP/Course.java
+++ Java OOP/Course.java
@@ -1,4 +1,15 @@
 public class Course {
     Integer Code;
     String CourseName;
+
+    public Course (Integer Code, String CourseName) {
+        this.Code = Code;
+        this.CourseName = CourseName;
+    }
+
+    public static void main(String[] args) {
+        Course uniCourse = new Course (737628, "Maths");
+        System.out.println(uniCourse.Code);
+        System.out.println(uniCourse.CourseName);
+    }
 }
