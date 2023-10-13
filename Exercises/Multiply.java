a912d67e23c10396dbbe4331f1c64bcfd5c17c0e
William-Brown5515 <william.brown@stfc.ac.uk>
2023-10-13 14:35:53 +0100

    13/10/2023 Exercise 6

--- Exercises/Multiply.java
+++ Exercises/Multiply.java
@@ -3,15 +3,21 @@ import java.util.Scanner;
 public class Multiply {
     public static void main(String[] args) {
         Scanner number = new Scanner(System.in);
-        System.out.print("Pick a number...     ");
-        Integer num1 = number.nextInt();
-        for (int i = 1; i <= 12; i = i + 1) {
-            System.out.print(i);
-            System.out.print(" x ");
-            System.out.print(num1);
-            System.out.print(" = ");
-            System.out.println(i * num1);
+        Boolean carry_on = true;
+        do {
+            System.out.print("Pick a number...     ");
+            Integer num1 = number.nextInt();
+            for (int i = 1; i <= 12; i = i + 1) {
+                System.out.print(i);
+                System.out.print(" x ");
+                System.out.print(num1);
+                System.out.print(" = ");
+                System.out.println(i * num1);
+            }
+            System.out.print("Would you like to do it again?    ");
+            carry_on = number.nextBoolean();
         }
+        while (carry_on = true);
         number.close();
     }
 }
