eca2d9caff639902677e82e2186e4e0c3e8f9ae5
William-Brown5515 <william.brown@stfc.ac.uk>
2023-10-13 14:08:47 +0100

    13/10/2023 Exercise 4

---
+++ Exercises/Multiply.java
@@ -0,0 +1,9 @@
+public class Multiply {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 12; i = i + 1) {
+            System.out.print(i);
+            System.out.print(" x 7 = ");
+            System.out.println(i * 7);
+        }
+    }
+}
