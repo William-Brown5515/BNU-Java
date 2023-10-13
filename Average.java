5d17e36411999c2e49f6f52cfc3ddd9ef3dd24d8
William-Brown5515 <william.brown@stfc.ac.uk>
2023-10-13 09:16:15 +0100

    06/10/2023 Exercise 4

---
+++ Average.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Average {
+    public static void main(String[] args) {
+        Scanner ave = new Scanner(System.in);
+        System.out.print("Please enter a number ");
+        Float num1 = ave.nextFloat();
+        System.out.print("Please enter another number ");
+        Float num2 = ave.nextFloat();
+        Float sum = num1 + num2;
+        Float result = sum / 2;
+        System.out.print("The average of the two numbers is " + result);
+    }
+}
