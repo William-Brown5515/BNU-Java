35f46ad29eb703c59b12f99e0ba5c99b00ba46e3
William-Brown5515 <william.brown@stfc.ac.uk>
2023-10-13 16:44:19 +0100

    20/10/2023 Exercises 4 & 5

--- Java OOP/Main.java
+++ Java OOP/Main.java
@@ -1,4 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        Student a = new Student(22233575, "Wil");
+        System.out.println(a.getID());
+        System.out.println(a.getName());
     }
 }
