d33108cb445d9e701ff1b4bdd814e22035374ad3
William-Brown5515 <william.brown@stfc.ac.uk>
2023-10-13 13:42:30 +0100

    13/10/2023 Exercise 1

--- Exercises/YearBorn.java
+++ Exercises/YearBorn.java
@@ -5,9 +5,14 @@ public class YearBorn {
         Scanner year = new Scanner(System.in);
         System.out.print("How old are you?   ");
         Integer age = year.nextInt();
-        Integer year_born = (2023 - age);
-        System.out.print("You were born in the year ");
-        System.out.print(year_born);
+        Integer year_born = (2024 - age);
+        System.out.print("You were probably born in the year ");
+        System.out.println(year_born);
+        if (age > 17) {
+            System.out.println("You are an adult");
+        } else {
+            System.out.println("You are a child");
+        }
         year.close();
     }
 }
