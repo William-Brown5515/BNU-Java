922a7e84aa5c0dbbbadddff2fb61573035983002
William-Brown5515 <william.brown@stfc.ac.uk>
2023-10-13 14:18:27 +0100

    13/10/2023 Exercise 5

--- Exercises/Multiply.java
+++ Exercises/Multiply.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
+
 public class Multiply {
     public static void main(String[] args) {
+        Scanner number = new Scanner(System.in);
+        System.out.print("Pick a number...     ");
+        Integer num1 = number.nextInt();
         for (int i = 1; i <= 12; i = i + 1) {
             System.out.print(i);
-            System.out.print(" x 7 = ");
-            System.out.println(i * 7);
+            System.out.print(" x ");
+            System.out.print(num1);
+            System.out.print(" = ");
+            System.out.println(i * num1);
         }
+        number.close();
     }
 }
