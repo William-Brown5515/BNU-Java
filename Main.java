455a09b85dbab2fc0d7a48ec47352a48d0f389ad
William-Brown5515 <william.brown@stfc.ac.uk>
2023-10-13 08:31:33 +0100

    06/10/2023 Exercise One

--- Main.java
+++ Main.java
@@ -1,9 +1,8 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) {      
         System.out.println("Hello World");
         String name = "Callum";
         System.out.print("Hello ");
         System.out.print(name);
     }
 }
-
