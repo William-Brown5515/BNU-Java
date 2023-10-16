488f4cbc62a1915eb46ecc183cc619c9f1f0a69a
William-Brown5515 <william.brown@stfc.ac.uk>
2023-10-16 09:43:17 +0100

    20/10/2023 Exercise 8

--- Java OOP/Main.java
+++ Java OOP/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Student a = new Student(22233575, "Wil");
+        Student a = new Student(22233575, "Will", 78924, "English");
         System.out.println(a.getID());
         System.out.println(a.getName());
     }
--- Java OOP/Student.java
+++ Java OOP/Student.java
@@ -1,16 +1,20 @@
 public class Student {
     Integer ID;
     String Name;
+    Course Course;
 
-    public Student (Integer ID, String Name) {
+    public Student (Integer ID, String Name, Integer Code, String CourseName) {
         this.ID = ID;
         this.Name = Name;
+        this.Course = new Course(Code, CourseName);
     }
 
     public static void main(String[] args) {
-        Student student = new Student(22233575, "Will");
+        Student student = new Student(22233575, "Will", 64255, "Maths");
         System.out.println(student.Name);
         System.out.println(student.ID);
+        System.out.println(student.Course.CourseName);
+        System.out.println(student.Course.Code);
     }
 
     public String getName() {
