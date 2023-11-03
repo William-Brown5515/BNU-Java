2fefc003ad411bc9262dfdf545600fa25204d1b1
William-Brown5515 <william.brown@stfc.ac.uk>
2023-11-03 10:51:58 +0000

    03/11/2023 Exercises 1 & 2

--- Java OOP Products/Product.java
+++ Java OOP Products/Product.java
@@ -9,4 +9,36 @@ public class Product {
         this.quantity = quantity;
     }
     
+    public void printProduct() {
+        System.out.print("The ID of the product is:   ");
+        System.out.println(this.id);
+        System.out.print("The name of this product is:   ");
+        System.out.println(this.name);
+        System.out.print("The available quantity of this product is:   ");
+        System.out.println(this.quantity);
+    }
+
+    public void setID(Integer id) {
+        this.id = id;
+    } 
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Integer getID() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Integer getQuantity() {
+        return this.quantity;
+    }
 }
